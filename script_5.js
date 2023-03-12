c69b63969dd0d07cc8de896a604a6dfa9b0de123
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-03-12 22:07:35 +0530

    Temporal Dead Zone

---
+++ script_5.js
@@ -0,0 +1,8 @@
+// 'let'  &  'const' are store in a different memory space 
+// we can not access 'let'  &  'const' declaration before we have put some value in them 
+
+console.log(x);
+
+let a = 10;
+
+var b = 100;
