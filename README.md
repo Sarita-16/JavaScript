a77cd19d5173a1fbb9dc6594a08e6c68b491ac48
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-03-12 19:12:58 +0530

    scope chain

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # JavaScript
+
 JavaScript Practice Code
+
+# script_1.js -->
+
+Functions
+
+# script_2.js -->
+
+Call Stack & Global Execution Context
+
+# script_3.js -->
+
+loosely type language
+
+# script_4.js -->
+
+Scope Chain
--- script_4.js
+++ script_4.js
@@ -1,10 +1,14 @@
-function a(){
+// Scope Chain
+
+// GEC Lexical Environment = NULL
+
+function a(){   // a() Lexical Environment = Local memory + GEC Lexical Environment
     var b = 10
 
     c()
-    function c(){
+    function c(){       // c() Lexical Environment = Local memory + a() Lexical Environment
         console.log(b)
     }
 }
 
-a()
+a() 
