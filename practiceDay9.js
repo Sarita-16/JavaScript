abf9113babf089b3728e31a55b659e8225b05465
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-04-04 21:20:56 +0530

    Display the current day and time

--- practiceDay9.js
+++ practiceDay9.js
@@ -4,5 +4,21 @@ function add() {
   let c = a + b;
   console.log(c);
 }
-
 add();
+
+// Display the current day and time
+
+let today = new Date();
+console.log("India Standard Time : " + today);
+let day = today.getDate();
+console.log("Day in Number : " + day);
+dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+console.log("Today Is : " + dayList[day]);
