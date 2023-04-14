3f4f0507094ba6272d8f1c993fde6640976118a6
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-04-14 22:54:18 +0530

    binary calculator

---
+++ binaryCalculator.js
@@ -0,0 +1,69 @@
+let item = document.getElementById("result");
+
+function checkOperation() {
+  if (
+    item.innerHTML.endsWith("+") ||
+    item.innerHTML.endsWith("-") ||
+    item.innerHTML.endsWith("*") ||
+    item.innerHTML.endsWith("/")
+  ) {
+    return true;
+  }
+}
+
+function clickZero() {
+  item.innerHTML += "0";
+  //console.log("clickZero : " + item.innerHTML);
+}
+
+function clickOne() {
+  item.innerHTML += "1";
+}
+
+function clickSum() {
+  if (item.innerHTML.length != 0 && !checkOperation()) {
+    item.innerHTML += "+";
+  }
+}
+
+function clickSub() {
+  if (item.innerHTML.length != 0 && !checkOperation()) {
+    item.innerHTML += "-";
+  }
+}
+
+function clickMul() {
+  if (item.innerHTML.length != 0 && !checkOperation()) {
+    item.innerHTML += "*";
+  }
+}
+
+function clickDiv() {
+  if (item.innerHTML.length != 0 && !checkOperation()) {
+    item.innerHTML += "/";
+  }
+}
+
+function clickClr() {
+  item.innerHTML = "";
+  //console.log("clickClr : " + item.innerHTML);
+}
+
+function clickEql() {
+  if (!checkOperation()) {
+    if (item.innerHTML.includes("+")) {
+      let eleArray = item.innerHTML.split("+");
+      let sum = 0;
+      for (let i = 0; i < eleArray.length; i++) {
+        sum = (parseInt(sum, 2) + parseInt(eleArray[i], 2)).toString(2);
+      }
+      document.getElementById("result").innerHTML = sum;
+    } else if (item.innerHTML.includes("-")) {
+      let eleArray = item.innerHTML.split("-");
+      let sub = eleArray[0] - eleArray[1];
+      document.getElementById("result").innerHTML = sub;
+    } else {
+      alert("Line must end with numbers 😉");
+    }
+  }
+}
---
+++ buttonsContainer.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  let bt5 = document.getElementById("btn5");
+}
