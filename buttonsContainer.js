973517adf471ea4ba79cf1104a5a1d2c83a89ffc
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-04-15 22:35:51 +0530

    fix some issue of button_Container

--- buttonsContainer.js
+++ buttonsContainer.js
@@ -1,5 +1,4 @@
-document.addEventListener("click", clickHandler);
-
-function clickHandler() {
-  let bt5 = document.getElementById("btn5");
-}
+let button = document.getElementById("btn5");
+button.onclick = function () {
+  document.getElementById("btn5").className = "rotated";
+};
