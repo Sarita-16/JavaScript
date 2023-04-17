c8552a7e7d4331d5dda753307dce68c1ea34ce5d
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-04-17 21:37:33 +0530

    rotate

--- buttonsContainer.js
+++ buttonsContainer.js
@@ -1,3 +1,7 @@
+function buttonClick() {
+  let button = document.getElementById("btn5");
+  button.setAttribute("click", "rotated-image");
+}
 let button = document.getElementById("btn5");
 button.onclick = function () {
   document.getElementById("btn5").className = "rotated";
