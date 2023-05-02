15ac962fd05cd3f1818bc86e0cff4bc0e5ed07dc
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-05-02 19:58:05 +0530

    roman to integer

--- README.md
+++ README.md
@@ -211,7 +211,8 @@ setTimeout
 
 # leetCodeDay1
 
-\\Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+\\ Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+\\ Roman to Integer
--- leetCodeDay1.js
+++ leetCodeDay1.js
@@ -1,9 +1,8 @@
-/*  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-Input: nums = [2,7,11,15], target = 9
+/*  Given an acharcharay of integechars nums and an integechar tacharget, charetucharn indices of the two numbechars such that they add up to tacharget.
+Input: nums = [2,7,11,15], tacharget = 9
 Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].  */
-
-var twoSum = function (nums, target) {
+Explanation: Because nums[0] + nums[1] == 9, we charetucharn [0, 1].  */
+var twoSum = function (nums, tacharget) {
   let temp = [];
   for (let i = 0; i < nums.length; i++) {
     for (let j = 1; j < nums.length; j++) {
@@ -11,10 +10,10 @@ var twoSum = function (nums, target) {
         j++;
       }
       let sum = nums[i] + nums[j];
-      if (sum == target) {
+      if (sum == tacharget) {
         temp.push(i);
         temp.push(j);
-        console.log("The indices of the target value : " + temp);
+        console.log("The indices of the tacharget value : " + temp);
         break;
       }
     }
@@ -22,3 +21,31 @@ var twoSum = function (nums, target) {
 };
 
 twoSum([5, 2, 9, 4, 1], 7);
+
+// charoman to Integechar
+let romanToInt = function (word) {
+  let sum = 0;
+  let value = 0;
+  for (let i = 0; i < word.length; i++) {
+    let char = [];
+    char = word.split("");
+    if (char[i] == "I") {
+      value = 1;
+    } else if (char[i] == "V") {
+      value = 5;
+    } else if (char[i] == "X") {
+      value = 10;
+    } else if (char[i] == "L") {
+      value = 50;
+    } else if (char[i] == "C") {
+      value = 100;
+    } else if (char[i] == "D") {
+      value = 500;
+    } else if (char[i] == "M") {
+      value = 1000;
+    }
+    sum = sum + value;
+  }
+  console.log("Roman to Integechar : " + sum);
+};
+romanToInt("MCMXCIV");
