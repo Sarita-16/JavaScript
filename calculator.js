cf2017aee053e58e183f20c195f182416fb2bbae
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-05-15 14:19:03 +0530

    calculator.css || calculator.css.map || calculator.html || calculator.js || calculator.scss are added.

--- calculator.js
+++ calculator.js
@@ -28,15 +28,20 @@ const compare = {
 
 buttons.forEach((item) => {
   item.onclick = () => {
-    debugger;
     if (item.id == "clear") {
       display.innerText = "";
     } else if (item.id == "backSpace") {
       let string = display.innerText.toString();
       display.innerText = string.substr(0, string.length - 1);
     } else if (display.innerText != "" && item.id == "equal") {
-      if (display.innerText.includes("%")) {
-        display.innerText = +display.innerText.replace("%", "") / 100;
+      debugger;
+      if (display.innerText.endsWith("%")) {
+        //display.innerText = +(display.innerText.replace("%", "") / 100);
+        let removePercent = display.innerText.replace("%", "");
+        let performPercent = +removePercent / 100;
+        display.innerText = parseFloat(performPercent.toFixed(3));
+      } else if (display.innerText.includes(".")) {
+        display.innerText = eval(display.innerText).toFixed(3);
       }
       display.innerText = eval(display.innerText);
     } else if (display.innerText == "" && item.id == "equal") {
