d799c4a0c2ccdb0823a6ba7aeb5a650de0dc2457
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-09-15 14:02:26 +0530

    valid palindrome

--- CodingForCampusing/Valid_Palindrome.js
+++ CodingForCampusing/Valid_Palindrome.js
@@ -1,2 +1,22 @@
-let validPalindrome = function (string) {};
-validPalindrome("A man, a plan, a canal: Panama");
+let validPalindrome = function (string) {
+  let str = string.split(" ").join("");
+  let newString = str.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+
+  let len = newString.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (newString[i] !== newString[len - 1 - i]) {
+      console.log(string + " is not a Valid Palindrome :(");
+      return 0;
+    }
+  }
+  console.log(string + " is a Valid Palindrome :)");
+  return 0;
+};
+validPalindrome("0P");
+
+/*
+/take input/
+const string = prompt('Enter a string: ');
+/call the function/h
+const value = checkPalindrome(string);
+*/
