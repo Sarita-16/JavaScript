6f9f54469561b4fe3f2685395871aebcdff389cd
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-09-23 23:57:01 +0530

    rotated sorted array

---
+++ CodingForCampusing/Find_Minimum_in_Rotated_Sorted_Array.js
@@ -0,0 +1,5 @@
+let sortedArray = function (arr) {
+  let newArr = arr.sort();
+  console.log(newArr[0]);
+};
+sortedArray([3, 4, 5, 1, 2]);
--- CodingForCampusing/Search_2D_Matrix.js
+++ CodingForCampusing/Search_2D_Matrix.js
@@ -1,9 +1,18 @@
 let searchMatrix = function (matrix, target) {
-  let result = matrix.indexOf(target);
-  if (result === -1) {
-    console.log("Element is not found :)");
+  let flag = 0;
+  let row = matrix.length;
+  let column = matrix[0].length;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      if (target == matrix[i][j]) {
+        flag = 1;
+      }
+    }
+  }
+  if (flag == 1) {
+    console.log("Element is found :)");
   } else {
-    console.log("Element is found :(");
+    console.log("Element is not found :(");
   }
 };
 searchMatrix(
@@ -11,6 +20,7 @@ searchMatrix(
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
+    [10, 11, 12],
   ],
   5
 );
