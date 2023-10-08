f140ddb6cf350e991022c5b20096922ae5653e4a
Sarita-16 <99497579+Sarita-16@users.noreply.github.com>
2023-10-08 16:02:22 +0530

    object

---
+++ CodingForCampusing/object.js
@@ -0,0 +1,13 @@
+const obj = {
+  name: "Sarita",
+  age: 22,
+  favColor: "Blue",
+  "is-student": true,
+};
+console.log(obj);
+
+obj.hobby = "drawing";
+
+console.log(obj.name);
+console.log(obj["is-student"]);
+console.log(obj);
